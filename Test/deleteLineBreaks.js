87f964cb27e7f378e258cf188c2bc39de67f10a9
JSchmiegel <51242445+JSchmiegel@users.noreply.github.com>
2021-07-01 18:18:57 +0200

    deleted old experiment

--- Test/deleteLineBreaks.js
+++
@@ -1,6 +0,0 @@
-function deleteLineBreaks() {
-    var textarea = document.getElementById("textarea");
-    var tmpString = textarea.value;
-    tmpString = tmpString.replace(/[\n\r]/g,' ');
-    textarea.value = tmpString;
-}
