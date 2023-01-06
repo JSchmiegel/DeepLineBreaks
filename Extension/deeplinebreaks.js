e7a88fd4f018a32ed67427891f1a10a792f6b810
JSchmiegel <git@jschmiegel.de>
2023-01-06 13:36:10 +0100

    fixed the positioning of the button

--- Extension/deeplinebreaks.js
+++ Extension/deeplinebreaks.js
@@ -1,9 +1,9 @@
-var textareaAButtonBar = document.getElementById("source-dummydiv").parentNode.nextElementSibling;
+var toolbarBar = document.getElementsByClassName("lmt__source_toolbar lmt__source_toolbar--visible");
 var newBtn = document.createElement("a");
 newBtn.id = "btnDeepLineBreak";
-newBtn.innerHTML = "<img src='https://addons.mozilla.org/user-media/addon_icons/2710/2710544-64.png?modified=cbfd9850' height='22'>";
-newBtn.style = "cursor: pointer; position: absolute; left: 60px; top: 5px;";
-textareaAButtonBar.appendChild(newBtn);
+newBtn.innerHTML = "<img src='https://addons.mozilla.org/user-media/addon_icons/2710/2710544-64.png?modified=cbfd9850' style='height:30px;'>";
+newBtn.style = "cursor: pointer; position: absolute; left: 20px;";
+toolbarBar[0].appendChild(newBtn);
 
 document.getElementById("btnDeepLineBreak").addEventListener("click", function insertTest() {
     var oldUrl = document.location.href
