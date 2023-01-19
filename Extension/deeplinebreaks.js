6082fb02cbba102ae2f63f52c2d7a652d7f41b8b
JSchmiegel <git@jschmiegel.de>
2023-01-19 18:32:26 +0100

    changed location of button

--- Extension/deeplinebreaks.js
+++ Extension/deeplinebreaks.js
@@ -2,7 +2,7 @@ var toolbarBar = document.getElementsByClassName("lmt__source_toolbar lmt__sourc
 var newBtn = document.createElement("a");
 newBtn.id = "btnDeepLineBreak";
 newBtn.innerHTML = "<img src='https://addons.mozilla.org/user-media/addon_icons/2710/2710544-64.png?modified=cbfd9850' style='height:30px;'>";
-newBtn.style = "cursor: pointer; position: absolute; left: 20px;";
+newBtn.style = "cursor: pointer; position: absolute; right: 20px;";
 toolbarBar[0].appendChild(newBtn);
 
 document.getElementById("btnDeepLineBreak").addEventListener("click", function insertTest() {
