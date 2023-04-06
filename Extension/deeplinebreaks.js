5b552434f45de44dab3453a9b6605d5d0f9402ab
JSchmiegel <git@jschmiegel.de>
2023-04-06 15:48:44 +0200

    fixed bug and changed placement of button

--- Extension/deeplinebreaks.js
+++ Extension/deeplinebreaks.js
@@ -2,18 +2,18 @@ var toolbarBar = document.getElementsByClassName("lmt__source_toolbar lmt__sourc
 var newBtn = document.createElement("a");
 newBtn.id = "btnDeepLineBreak";
 newBtn.innerHTML = "<img src='https://addons.mozilla.org/user-media/addon_icons/2710/2710544-64.png?modified=cbfd9850' style='height:30px;'>";
-newBtn.style = "cursor: pointer; position: absolute; right: 20px;";
+newBtn.style = "cursor: pointer; position: relative; right: 0%; top: -3px";
 toolbarBar[0].appendChild(newBtn);
 
 document.getElementById("btnDeepLineBreak").addEventListener("click", function insertTest() {
     var threshold = 1300;
-    var textareaObject = document.getElementsByClassName("lmt__textarea lmt__source_textarea lmt__textarea_base_style")[0];
+    var textareaObject = document.querySelector('p');
 
     if (getCharactersInTextarea(textareaObject) >= threshold) {
         // remove all linebreaks in the textarea
-        var tmpString = textareaObject.value;
+        var tmpString = textareaObject.textContent;
         tmpString = tmpString.replace(/[\n\r]/g,' ');
-        textareaObject.value = tmpString;
+        textareaObject.textContent = tmpString;
         textareaObject.focus();
     }else{
         // remove all linebreaks in the url
@@ -24,5 +24,5 @@ document.getElementById("btnDeepLineBreak").addEventListener("click", function i
 })
 
 function getCharactersInTextarea(textareaObject) {
-    return(textareaObject.value.length)
+    return(textareaObject.textContent.length)
 }
